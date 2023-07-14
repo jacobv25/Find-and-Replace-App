d3919f9e394f3a5ebfeaf0c910637435686e17bf
Jacob Valenzuela <32970534+jacobv25@users.noreply.github.com>
2023-07-14 10:01:10 -0700

    input is now performed through cli
    instead of two checkboxes and a button, the user will use a cli to enter commands

--- tk-version/main.py
+++ tk-version/main.py
@@ -7,42 +7,33 @@ class MyApp(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        self.search_box = tk.Entry(self)
-        self.search_box.insert(0, "Enter text to search...")
-
-        self.replace_box = tk.Entry(self)
-        self.replace_box.insert(0, "Enter replacement text...")
-
-        self.log_box = scrolledtext.ScrolledText(self)
-
-        self.search_label = tk.Label(self, text="Search Text")
-        self.replace_label = tk.Label(self, text="Replacement Text")
-        self.log_label = tk.Label(self, text="Log")
-
         self.editor = scrolledtext.ScrolledText(self)
-
-        self.search_button = tk.Button(self, text="Search and Replace", command=self.search_and_replace)
-
-        # Layout
-        self.search_label.pack()
-        self.search_box.pack()
-        self.replace_label.pack()
-        self.replace_box.pack()
-        self.search_button.pack()
         self.editor.pack()
-        self.log_label.pack()
-        self.log_box.pack()
 
-    def search_and_replace(self):
-        search_text = self.search_box.get()
-        replace_text = self.replace_box.get()
+        self.cli = scrolledtext.ScrolledText(self)
+        self.cli.bind('<Return>', self.execute_command)
+        self.cli.pack()
 
-        # Replace text and update log
-        text = self.editor.get(1.0, tk.END)
-        new_text = self.preserve_case_replace(text, search_text, replace_text)
-        self.editor.delete(1.0, tk.END)
-        self.editor.insert(1.0, new_text)
-        self.log_box.insert(tk.END, f"Replaced '{search_text}' with '{replace_text}'\n")
+    def execute_command(self, event=None):
+        # Get the last line in the CLI box
+        command = self.cli.get('end-2c linestart', 'end-1c')
+
+        if command.startswith("sr "):
+            _, search_text, replace_text = command.split(" ", 2)
+
+            # Check if search text is in the editor text
+            text = self.editor.get(1.0, tk.END)
+            if search_text not in text:
+                self.cli.insert(tk.END, f" | No matches for '{search_text}' found in text.\n")
+                return
+
+            # Replace text and update log
+            new_text = self.preserve_case_replace(text, search_text, replace_text)
+            self.editor.delete(1.0, tk.END)
+            self.editor.insert(1.0, new_text)
+            self.cli.insert(tk.END, f" | Replaced '{search_text}' with '{replace_text}'\n")
+        else:
+            self.cli.insert(tk.END, f" | Unknown command: {command}\n")
 
     def preserve_case_replace(self, original_text, search_text, replace_text):
         pattern = re.compile(re.escape(search_text), re.IGNORECASE)
